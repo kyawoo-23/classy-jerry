3552720a1c8df2ccf2ddbbb196d42f59aec80af6
kyawoo-23 <kyawkyawjek@gmail.com>
2022-09-14 14:32:23 +0630

    env var added

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -4,12 +4,12 @@ import { getAuth, GoogleAuthProvider } from "firebase/auth";
 import { getStorage } from 'firebase/storage'
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAQNTHQqrQjPCjQE4dngUlnarC8I8kEZFA",
-  authDomain: "classy-jerry.firebaseapp.com",
-  projectId: "classy-jerry",
-  storageBucket: "classy-jerry.appspot.com",
-  messagingSenderId: "231236747450",
-  appId: "1:231236747450:web:3c298936c2a4a009e10283"
+  apiKey: `${process.env.REACT_APP_API_KEY}`,
+  authDomain: `${process.env.REACT_APP_AUTH_DOMAIN}`,
+  projectId: `${process.env.REACT_APP_PROJECT_ID}`,
+  storageBucket: `${process.env.REACT_APP_STORAGE_BUCKET}`,
+  messagingSenderId: `${process.env.REACT_APP_MESSAGING_SENDER_ID}`,
+  appId: `${process.env.REACT_APP_APP_ID}`
 };
 
 // Initialize Firebase
